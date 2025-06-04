45e88c82c2393cc2a1ae0b7743486c35976af8d4
Shawn Law <shawnlaw6669@gmail.com>
2025-06-04 11:17:34 +0800

    tabs navigatioin

---
+++ src/app/(tabs)/likes.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function LikesScreen() {
+  return (
+    <View>
+      <Text>LikesScreen</Text>
+    </View>
+  )
+}
---
+++ src/components/PostListItem.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PostListItem = () => {
+  return (
+    <View>
+      <Text>PostListItem</Text>
+    </View>
+  )
+}
+
+export default PostListItem
+
+const styles = StyleSheet.create({
+
+})
