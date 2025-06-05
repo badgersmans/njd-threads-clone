4e103f3bf9d280243dd7af59445ab77a519664a0
Shawn Law <shawnlaw6669@gmail.com>
2025-06-05 16:34:20 +0800

    fetch supabase post!

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,20 +1,38 @@
 import PostListItem from '@/components/PostListItem'
 import { dummyPosts } from '@/dummyData'
+import { supabase } from '@/lib/supabase'
+import { Post } from '@/types'
 import { Link } from 'expo-router'
+import { useEffect, useState } from 'react'
 import { FlatList, Text } from 'react-native'
 
 export default function HomeScreen() {
+  const [posts, setPosts] = useState<Post[]>([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const {data, error} = await supabase.from('posts').select('*, user:profiles(*)')
+
+      if(error) {
+        console.log(error)
+      }
+      setPosts(data)
+    }
+
+    fetchPosts()
+  }, [])
+
+  console.log(JSON.stringify(posts, null, 2))
+
   return (
     <FlatList
-      data={dummyPosts}
+      data={posts}
       renderItem={({item}) => <PostListItem post={item} />}
       showsVerticalScrollIndicator={false}
       ListHeaderComponent={() => (
-        <>
-          <Link href={'/new'} className='text-red-300'>
-            New Post
-          </Link>
-        </>
+        <Link href={'/new'} className='text-red-300'>
+          New Post
+        </Link>
       )}
     />
   )
--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -13,7 +13,7 @@ export default function PostListItem({post}: {post: Post}) {
     <Link href={`/${post.id}`} asChild>
       <Pressable className="flex-row p-4 border-b border-gray-800/80">
         <Image
-          source={{ uri: post.user.image }} 
+          source={{ uri: post.user.avatar_url }} 
           className="w-12 aspect-square rounded-full"
         />
 
@@ -27,28 +27,28 @@ export default function PostListItem({post}: {post: Post}) {
 
           <Text className="text-white mt-2 leading-5">{post.content}</Text>
           
-          {post.parent && (
-            <View className="mt-2 bg-gray-800/50 p-3 rounded-lg">
+          {/* {post.parent && ( */}
+            {/* <View className="mt-2 bg-gray-800/50 p-3 rounded-lg">
               <View className="flex-row items-center">
-                <Text className="text-gray-300">{post.parent.user.name}</Text>
+                <Text className="text-gray-300">{post.user.username}</Text>
                 <Text className="text-gray-300 ml-2">·</Text>
                 <Text className="text-gray-300 ml-2">
-                  {new Date(post.parent.createdAt).toLocaleDateString()}
+                  {new Date(post.createdAt).toLocaleDateString()}
                 </Text>
               </View>
-              <Text className="text-gray-300 mt-1">{post.parent.content}</Text>
-            </View>
-          )}
+              <Text className="text-gray-300 mt-1">{post.content}</Text>
+            </View> */}
+          {/* )} */}
 
           <View className="flex-row items-center mt-3 gap-4">
             <View className='flex-row items-center gap-1'>
               <Ionicons name="heart-outline" size={21} color={"#d1d5db"}/>
-              <Text className="text-gray-300">{post.replies.length}</Text>
+              <Text className="text-gray-300">{0}</Text>
             </View>
 
             <View className='flex-row items-center gap-1'>
               <Ionicons name="chatbubble-outline" size={21} color={"#d1d5db"}/>
-              <Text className="text-gray-300">{post.replies.length}</Text>
+              <Text className="text-gray-300">{0}</Text>
             </View>
 
             <Ionicons name="repeat-outline" size={21} color={"#d1d5db"}/>
